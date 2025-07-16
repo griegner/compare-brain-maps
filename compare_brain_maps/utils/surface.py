eac184df8b6f28ae4cd7c2f93c8daba64b8cae8c
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-07-15 19:27:04 -0700

    compute adjacency matrix of the surface mesh

--- compare_brain_maps/utils/surface.py
+++ compare_brain_maps/utils/surface.py
@@ -1,7 +1,10 @@
 """utilities for surface maps"""
 
+import numpy as np
 from neuromaps.datasets import fetch_atlas
+from neuromaps.points import make_surf_graph
 from nilearn.surface import SurfaceImage
+from scipy.sparse import csr_array
 
 
 def load_surface_atlas(atlas: str, density: str, mesh: str) -> SurfaceImage:
@@ -31,3 +34,49 @@ def load_surface_atlas(atlas: str, density: str, mesh: str) -> SurfaceImage:
         mesh={"left": giftis[mesh].L, "right": giftis[mesh].R},
         data={"left": giftis["medial"].L, "right": giftis["medial"].R},
     )
+
+
+def _adjacency_matrix(faces, mask=None):
+    """return adjacency matrix for one hemisphere, excluding the medial wall"""
+    n_vertices = faces.max() + 1
+
+    edges = np.vstack([faces[:, [0, 1]], faces[:, [0, 2]], faces[:, [1, 2]]])
+    sorted_edges = np.sort(edges, axis=1)
+    unique_edges = np.unique(sorted_edges, axis=0)
+    i, j = unique_edges[:, 0], unique_edges[:, 1]
+
+    ones = np.ones(len(unique_edges) * 2, dtype=np.int8)
+    row_indices = np.concatenate([i, j])
+    col_indices = np.concatenate([j, i])
+
+    return csr_array((ones, (row_indices, col_indices)), shape=(n_vertices, n_vertices))
+
+
+def adjacency_matrices(atlas: str, density: str, mesh: str) -> dict:
+    """Adjacency matrices for the left and right hemispheres of a surface atlas.
+    Elements *i,j* are `1` if vertex pairs are adjacent, `0` otherwise.
+
+    Parameters
+    ----------
+    atlas : str
+        Atlas in `{"civet", "fsaverage", "fsLR"}`
+    density : str
+        civet in `{"41k", "164k"}`\\
+        fsaverage in `{"3k", "10k", "41k", "164k"}`\\
+        fsLR in `{"4k", "8k", "32k", "164k"}`
+    mesh : str
+        civet in `{"white", "midthickness", "inflated", "veryinflated", "sphere"}`\\
+        fsaverage in `{"white", "pial", "inflated", "sphere"}`\\
+        fsLR in `{"midthickness", "inflated", "veryinflated", "sphere"}`
+
+    Returns
+    -------
+    dict of scipy.sparse.csr_array
+        dictionary containing sparse adjacency matrices per hemisphere, excluding the medial wall:
+        - "left": csr_array for the left hemisphere
+        - "right": csr_array for the right hemisphere
+    """
+    surface_atlas = load_surface_atlas(atlas, density, mesh)
+    A_left = _adjacency_matrix(surface_atlas.mesh.parts["left"].faces)
+    A_right = _adjacency_matrix(surface_atlas.mesh.parts["right"].faces)
+    return {"left": A_left, "right": A_right}
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,25 +3,38 @@ import pytest
 
 from compare_brain_maps.utils import surface
 
+atlas_density_params = [
+    ("fsaverage", "3k"),
+    ("fsaverage", "10k"),
+    ("fsaverage", "41k"),
+    ("fsaverage", "164k"),
+    ("fsLR", "4k"),
+    ("fsLR", "8k"),
+    ("fsLR", "32k"),
+    ("fsLR", "164k"),
+    ("civet", "41k"),
+    ("civet", "164k"),
+]
 
-@pytest.mark.parametrize(
-    "atlas, density",
-    [
-        ("fsaverage", "3k"),
-        ("fsaverage", "10k"),
-        ("fsaverage", "41k"),
-        ("fsaverage", "164k"),
-        ("fsLR", "4k"),
-        ("fsLR", "8k"),
-        ("fsLR", "32k"),
-        ("fsLR", "164k"),
-        ("civet", "41k"),
-        ("civet", "164k"),
-    ],
-)
+
+@pytest.mark.parametrize("atlas, density", atlas_density_params)
 def test_load_surface_atlas(atlas, density):
-    """test all neuromaps atlases can be loaded as a polymesh"""
+    """test all neuromaps atlases can be loaded as a SurfaceImage"""
     surface_atlas = surface.load_surface_atlas(atlas=atlas, density=density, mesh="inflated")
-    assert len(surface_atlas.mesh.parts) == 2  # L/R hemispheres exist
-    assert np.round(surface_atlas.mesh.n_vertices / 2000) == float(density[:-1])  # correct density
-    assert surface_atlas.data.parts["left"].size > surface_atlas.data.parts["left"].sum()  # excludes medial wall
+    assert len(surface_atlas.mesh.parts) == 2, "missing hemisphere"
+    assert np.round(surface_atlas.mesh.n_vertices / 2000) == float(density[:-1]), "incorrect density"
+    assert surface_atlas.mesh.n_vertices == surface_atlas.data.shape[0], "shape mismatch between mesh and data"
+    assert surface_atlas.data.parts["left"].size > surface_atlas.data.parts["left"].sum(), "incorrect medial wall mask"
+
+
+@pytest.mark.parametrize("atlas, density", atlas_density_params)
+def test_surface_adjacency_matrices(atlas, density):
+    """test all neuromaps atlases have valid adjacency matrices"""
+    A = surface.adjacency_matrices(atlas=atlas, density=density, mesh="inflated")
+
+    for hemi in ["left", "right"]:
+        assert A[hemi].nnz > 0, f"{hemi} entries are all zero"
+        assert (A[hemi] != A[hemi].T).nnz == 0, f"{hemi} is asymmetric"
+        assert np.all(A[hemi].diagonal() == 0), f"{hemi} diagonals are non-zero"
+        sparsity = A[hemi].nnz / (A[hemi].shape[0] ** 2)
+        assert sparsity < 0.01, f"{hemi} is not sparse"
