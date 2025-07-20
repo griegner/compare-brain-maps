01e649f483ed306bafc5a8c4142c812c57b7f8e4
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-07-20 15:32:18 -0700

    inherit from  to allow nilearn surface plotting

--- compare_brain_maps/utils/surface.py
+++ compare_brain_maps/utils/surface.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from neuromaps.datasets import fetch_atlas
-from nilearn.surface import PolyData, PolyMesh
+from nilearn.surface import PolyData, SurfaceImage, load_surf_data
 from nilearn.surface.surface import _check_data_and_mesh_compat
 from scipy.sparse import csr_array
 
@@ -16,9 +16,9 @@ def _adjacency(faces, mask=None):
     unique_edges = np.unique(sorted_edges, axis=0)
 
     if mask is not None:
-        (idx,) = np.where(mask)
-        mask = np.any(np.isin(unique_edges, idx), axis=1)
-        unique_edges = unique_edges[mask]
+        # filter out edges were either vertex is part of the medial wall
+        valid_edges = mask[unique_edges[:, 0]] & mask[unique_edges[:, 1]]
+        unique_edges = unique_edges[valid_edges]
 
     i, j = unique_edges[:, 0], unique_edges[:, 1]
 
@@ -29,7 +29,7 @@ def _adjacency(faces, mask=None):
     return csr_array((ones, (row_indices, col_indices)), shape=(n_vertices, n_vertices))
 
 
-class Surface:
+class Surface(SurfaceImage):
     """Surface object containing data, mesh, and medial wall mask for both hemispheres.
 
     Parameters
@@ -42,18 +42,18 @@ class Surface:
         civet in `{"41k", "164k"}`\\
         fsaverage in `{"3k", "10k", "41k", "164k"}`\\
         fsLR in `{"4k", "8k", "32k", "164k"}`\\
-        *note: {3k=2562, 4k=4002, 8k=7842, 10k=10242, 32k=32492, 41k=40962, 164k=163842}
+        *note: `{3k=2562, 4k=4002, 8k=7842, 10k=10242, 32k=32492, 41k=40962, 164k=163842}`
     surface : str, optional
-        civet in `{"white", "midthickness", "inflated", "veryinflated", "sphere"}`\\
+        civet in `{"white", "midthickness", "inflated","veryinflated",  "sphere"}`\\
         fsaverage in `{"white", "pial", "inflated", "sphere"}`\\
-        fsLR in `{"midthickness", "inflated", "veryinflated", "sphere"}`
+        fsLR in `{"midthickness", "inflated", "sphere"}`
 
     Attributes
     ----------
     data : nilearn.surface.PolyData
         surface data for both hemispheres
     medial : nilearn.surface.PolyData
-        medial wall mask for both hemispheres
+        surface mask (excluding medial wall) for both hemispheres
     mesh : nilearn.surface.PolyMesh
         surface meshes (vertices and faces) for both hemispheres
     shape : tuple
@@ -62,33 +62,29 @@ class Surface:
     Examples
     --------
     >>> import numpy as np
+    >>> from compare_brain_maps.utils import Surface
     >>> n_vertices = 2562
     >>> data = {
     ...     "left": np.random.randn(n_vertices),
     ...     "right": np.random.randn(n_vertices)
     ... }
-    >>> surf = Surface(data, atlas="fsaverage", density="3k", surface="pial")
-    >>> surf.shape
+    >>> X = Surface(data, atlas="fsaverage", density="3k", surface="pial")
+    >>> X.shape
     (5124,)
     """
 
     def __init__(self, data, atlas="fsaverage", density="3k", surface="pial"):
         giftis = fetch_atlas(atlas, density)
+        super().__init__(data=data, mesh={"left": giftis[surface].L, "right": giftis[surface].R})
 
-        self.data = PolyData(**data)
-        self.medial = PolyData(left=giftis["medial"].L, right=giftis["medial"].R)
-        self.mesh = PolyMesh(left=giftis[surface].L, right=giftis[surface].R)
+        medial = {}
+        for hemi, gifti in zip(["left", "right"], giftis["medial"]):
+            mask = load_surf_data(gifti).astype(bool)
+            medial[hemi] = mask
+            self.data.parts[hemi][~mask] = np.nan
+        self.medial = PolyData(**medial)
 
-        _check_data_and_mesh_compat(self.mesh, self.data)
         _check_data_and_mesh_compat(self.mesh, self.medial)
-
-    @property
-    def shape(self):
-        """number of vertices for l+r hemispheres"""
-        return self.data.shape
-
-    def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} {self.shape}>"
 
     def get_adjacency(self):
         """Adjacency matrices for the left and right hemispheres of a surface atlas.
