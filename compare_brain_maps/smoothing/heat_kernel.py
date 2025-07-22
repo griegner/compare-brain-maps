1d2b0e3597272545c1f0d0b859d1e40c93a686c9
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-07-22 15:50:52 -0700

    add `HeatKernelSmoother` and tests

--- compare_brain_maps/smoothing/heat_kernel.py
+++ compare_brain_maps/smoothing/heat_kernel.py
@@ -1,5 +1,50 @@
-from sklearn.base import TransformerMixin, BaseEstimator
+import copy
+
+import numpy as np
+from lapy import Solver, TriaMesh
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class HeatKernelSmoother(TransformerMixin, BaseEstimator):
-    pass
+    """Heat kernel smoothing on a surface mesh using Laplace-Beltrami eigenvalues and eigenmodes.
+
+    Parameters
+    ----------
+    sigma : float, optional
+        smoothing parameter controlling the width of the heat kernel, by default 1.0
+    n_modes : int, optional
+        number of eigenmodes, by default 500
+    """
+
+    def __init__(self, sigma=1.0, n_modes=500):
+        self.sigma = sigma
+        self.n_modes = n_modes
+
+    def fit(self, X):
+        """For scikit-learn compatibility."""
+        return self
+
+    def transform(self, X):
+        """Apply heat kernel smoothing to `Surface.data`.
+
+        Parameters
+        ----------
+        X : Surface
+            surface object containing the data and mesh for both hemispheres
+
+        Returns
+        -------
+        Surface
+            surface object with data smoothed on the surface mesh
+        """
+        X_smoothed = copy.deepcopy(X)
+
+        for hemi in ["left", "right"]:
+            mesh = X_smoothed.mesh.parts[hemi]
+            tria_mesh = TriaMesh(v=mesh.coordinates, t=mesh.faces)
+            fem = Solver(geometry=tria_mesh, use_cholmod=True)  # require scikit-sparse
+            evals, emodes = fem.eigs(k=self.n_modes + 1)
+            evals, emodes = evals[1:], emodes[:, 1:]  # first is non-constant
+            beta_ = np.linalg.solve((emodes.T @ emodes), emodes.T @ X_smoothed.data.parts[hemi])
+            X_smoothed.data.parts[hemi] = np.sum(np.exp(-evals * self.sigma) * beta_ * emodes, axis=1)
+        return X_smoothed
--- tests/test_smoothing.py
+++ tests/test_smoothing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from compare_brain_maps.smoothing import NearestNeighborSmoother
+from compare_brain_maps.smoothing import HeatKernelSmoother, NearestNeighborSmoother
 from compare_brain_maps.utils import Surface
 
 atlas_density_params = [
@@ -32,5 +32,19 @@ def test_nearest_neighbor_smoother(atlas, density, n_vertices):
 
     for hemi in ["left", "right"]:
         assert not np.array_equal(X.data.parts[hemi], X_smoothed.data.parts[hemi]), f"{hemi} smoothing not applied"
-        np.testing.assert_almost_equal(X_smoothed.data.parts[hemi].mean(), 0, err_msg=f"{hemi} mean not zero")
-        np.testing.assert_almost_equal(X_smoothed.data.parts[hemi].std(), 1, err_msg=f"{hemi} std not one")
+
+
+@pytest.mark.parametrize("atlas, density, n_vertices", [("fsaverage", "3k", 2562), ("fsLR", "4k", 4002)])
+def test_heat_kernel_smoother(atlas, density, n_vertices):
+    rng = np.random.default_rng(seed=0)
+    data = {"left": rng.standard_normal(n_vertices), "right": rng.standard_normal(n_vertices)}
+    data = {key: (values - np.mean(values)) / np.std(values) for key, values in data.items()}  # standardize
+
+    smoother = HeatKernelSmoother()
+    X = Surface(data=data, atlas=atlas, density=density, surface="inflated", mask_medial=False)
+    X_smoothed = smoother.transform(X)
+
+    assert isinstance(X_smoothed, Surface), "Surface object not returned"
+
+    for hemi in ["left", "right"]:
+        assert not np.array_equal(X.data.parts[hemi], X_smoothed.data.parts[hemi]), f"{hemi} smoothing not applied"
